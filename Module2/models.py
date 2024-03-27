71bd3e1a79a2611f4a6223b21f4bf323f7d82ff4
tejaswariboda <tejaswariboda@gmail.com>
2024-03-27 22:14:45 +0530

    Your commit message here

---
+++ Module2/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+class URL(models.Model):
+    long_url = models.URLField()
+    short_url = models.CharField(max_length = 20, unique=True)
